18fabe9848654d80a814be61823939f2b4e5261a
Naotiki <naotiki@naotiki-apps.xyz>
2022-05-14 02:40:53 +0900

    くそねむい

---
+++ scripts/HiraganaConverter.ts
@@ -0,0 +1,34 @@
+const hiraganaPair: { [key: string]: string }={
+  ぁ:"あ",
+  ぃ:"い",
+  ぅ :"う",
+  ぇ:"え",
+  ぉ :"お",
+  っ:"つ",
+  ゃ:"や",
+  ゅ:"ゆ",
+  ょ:"よ",
+  ゎ:"わ",
+  を:"お",
+  ヶ:"け",
+  ッ:"つ",
+}
+
+export default function toUpper (hiragana:any){
+  if (Object.keys(hiraganaPair).includes(hiragana)) {
+    return  hiraganaPair[hiragana]
+  }else return hiragana
+}
+
+export function hiraToKana(str:string) :string{
+  return str.replace(/[\u3041-\u3096]/g, function (match) {
+    const chr = match.charCodeAt(0) + 0x60;
+    return String.fromCharCode(chr);
+  });
+}
+export function kanaToHira(str:string) :string{
+  return str.replace(/[\u30A1-\u30F6]/g, function (match) {
+    const chr = match.charCodeAt(0) - 0x60;
+    return String.fromCharCode(chr);
+  });
+}
---
+++ types/siritori.d.ts
@@ -0,0 +1,18 @@
+type WordHistory={
+  word:string,
+  pageId:string|null
+}
+
+type Dir = 'left' | 'right'
+type BalloonSay={
+  text:string,
+  dir:Dir,
+  url?:string
+}
+
+type HiraganaAPI={
+  converted:string
+}
+type WikipediaApi={
+
+}
